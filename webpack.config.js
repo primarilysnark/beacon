d2c4206e85c22cdcdc9124a07a4865278c6c7114
Primarily Snark <starkey.joshua@gmail.com>
2016-04-25 19:37:42 -0700

    Add initial build tooling.
    Added initial build tooling to support building beacon with webpack
    and eslint. Also setup initial support for using React and module-
    local style sheets.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry:  './src',
+  output: {
+    path:     'dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    preLoaders: [
+        {
+            test: /\.jsx?$/,
+            loader: 'eslint',
+        }
+    ],
+    loaders: [
+      {
+        test: /\.(jpg|jpeg)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 100000
+        },
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!less-loader'),
+      },
+      {
+        test:   /\.jsx?$/,
+        excluse: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+      {
+        test: require.resolve("react"),
+        loader: "expose?React"
+      }
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      allChunks: true,
+    }),
+  ],
+};
